0b14e6842bfb18f7f42fa5530b70cf9bcebbe63d
karthik-buk <karthikn503@gmail.com>
2023-07-20 19:36:59 +0530

    📦Added required packages and added network details on hardhat config🔧

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,31 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-	solidity: "0.8.19",
+	solidity: {
+		version: "0.8.19",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 10000,
+			},
+		},
+	},
+	networks: {
+		mumbai: {
+			url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}`,
+			accounts:
+				process.env.PRIVATE_KEY_MUMBAI !== undefined
+					? [process.env.PRIVATE_KEY_MUMBAI]
+					: [],
+		},
+	},
+	gasReporter: {
+		enabled: process.env.REPORT_GAS !== undefined,
+		currency: "USD",
+	},
+	etherscan: {
+		apiKey: process.env.POLYGONSCAN_API_KEY,
+	},
 };
 
 export default config;
