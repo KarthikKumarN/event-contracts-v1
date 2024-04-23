ee5313bd74cac86efa0a8fbf7990ccb971f81881
karthik-buk <karthikn503@gmail.com>
2024-04-23 14:12:48 +0530

    +Added EventNFT deployer contract

---
+++ contracts/BukEventDeployer/IBukEventDeployer.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+interface IBukEventDeployer {
+    event DeployedEventNFT(
+        address indexed eventNFT,
+        address indexed eventDeployer
+    );
+
+    /**
+     * @dev Emitted when new BukEventProtocol address has been updated
+     * @param newAddress, Address of the new bukProtocol
+     */
+    event SetBukEventProtocol(address newAddress);
+
+    /**
+     * @dev Function will set new BUK protocol address
+     * @param _bukProtocol address of new BUK protocol
+     */
+    function setBukEventProtocol(address _bukProtocol) external;
+
+    function deployEventNFT(
+        string calldata _name,
+        address _bukEventProtocol,
+        address _bukTreasury
+    ) external returns (address);
+}
